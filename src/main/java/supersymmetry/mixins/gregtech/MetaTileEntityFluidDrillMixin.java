092ea9212aae3a75cb4d617a6adabcfca7d31ecb
PatRat2 <olchowieckipatryk@gmail.com>
2025-09-14 23:35:26 +0100

    Fixed mixin json, and fluid drilling rig now shows a warning message
    Forgor to add 2 mixins in the last commit, and the fluid drilling rig now actually displays a message that it cant pump fluids that have extra requirements

--- src/main/java/supersymmetry/mixins/gregtech/MetaTileEntityFluidDrillMixin.java
+++ src/main/java/supersymmetry/mixins/gregtech/MetaTileEntityFluidDrillMixin.java
@@ -10,6 +10,8 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,6 +19,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
+
+import supersymmetry.api.BedrockFluidRequirementRegistry;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.Style;
+import net.minecraftforge.fluids.Fluid;
+import gregtech.api.worldgen.bedrockFluids.BedrockFluidVeinHandler;
+import gregtech.api.capability.impl.FluidDrillLogic;
 
 @Mixin(value = MetaTileEntityFluidDrill.class, remap = false)
 public abstract class MetaTileEntityFluidDrillMixin extends MultiblockWithDisplayBase {
@@ -39,5 +50,22 @@ public abstract class MetaTileEntityFluidDrillMixin extends MultiblockWithDispla
     @Override
     public boolean isMultiblockPartWeatherResistant(@Nonnull IMultiblockPart part) {
         return true;
+    }
+
+    @Shadow private FluidDrillLogic minerLogic;
+
+    @Inject(method = "addWarningText", at = @At("TAIL"))
+    private void addRequiredFluidWarning(List<ITextComponent> textList, CallbackInfo ci) {
+
+        if (minerLogic == null) return;
+
+        Fluid drilledFluid = minerLogic.getDrilledFluid();
+        if (drilledFluid == null) return;
+
+        BedrockFluidVeinHandler.FluidVeinWorldEntry entry = BedrockFluidVeinHandler.getFluidVeinWorldEntry(((MetaTileEntityFluidDrill) (Object) this).getWorld(), minerLogic.getChunkX(), minerLogic.getChunkZ());
+        if (entry != null && BedrockFluidRequirementRegistry.hasRequirement(entry.getDefinition().getDepositName())) {
+            textList.add(new TextComponentString("Not :goog:... multi needs fluid input..")
+                    .setStyle(new Style().setColor(TextFormatting.RED)));
+        }
     }
 }
